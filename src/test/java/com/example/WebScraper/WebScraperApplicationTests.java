4d517a9736153c121eebdb53998c794a8217b36d
Sargis Sargsyan <sargiss987@gmail.com>
2022-08-10 18:14:14 +0400

    init project

---
+++ src/test/java/com/example/WebScraper/WebScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebScraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
